d1d4ff781cf409fa5f41b9a1fb5b1c75d1663f5e
LENOVO <MeutyaAzzahrani>
2024-05-13 15:53:31 +0700

    membuat modifier akses pada class mahasiswa

--- construktor.cpp
+++ construktor.cpp
@@ -2,3 +2,22 @@
 #include <string>
 using namespace std;
 
+class Mahasiswa
+{
+public:
+    int nim;
+    string nama;
+
+    Mahasiswa()
+    {
+        nim = 0;
+        nama = "";
+    };
+    Mahasiswa(int iNim)
+    {
+    nim = iNim;
+    };
+
+    Mahasiswa(string);
+    Mahasiswa(int iNim, string iNama);
+};
