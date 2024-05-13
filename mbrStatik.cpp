4433c14f84650c7c0d736a704989ea95e34f385c
LENOVO <MeutyaAzzahrani>
2024-05-13 14:50:19 +0700

    modifier public

--- mbrStatik.cpp
+++ mbrStatik.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+class mahasiswa{
+ public:
+   class mahasiswa{
+ public:
+    static int nim;
+    int id;
+    string nama;
+
+};
+
 
